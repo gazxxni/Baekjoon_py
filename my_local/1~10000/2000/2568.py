fbd5c03ffa3b0523c36ade00b4f43fc68d1f24f8
gazxxni <jungsj0524@gmail.com>
2025-09-25 12:28:32 +0900

    a

--- my_local/1~10000/2000/2568.py
+++ my_local/1~10000/2000/2568.py
@@ -6,17 +6,17 @@ arr = []
 for _ in range(n):
     a, b = map(int, input().split())
     arr.append((a, b))
-
 arr.sort()
+brr = []
+for a, b in arr:
+    brr.append(b)
 
-brr = [b for a, b in arr]
+tails = []
+tails_pos = []
 
-tails = []       
-tails_idx = []    
-pos = [0] * n      
-prev_idx = [-1] * n  
+prev_idx = [-1] * n
 
-def lower_bound(a, x):
+def search(a, x):
     lo, hi = 0, len(a)
     while lo < hi:
         mid = (lo + hi) // 2
@@ -27,39 +27,29 @@ def lower_bound(a, x):
     return lo
 
 for i, x in enumerate(brr):
-    j = lower_bound(tails, x) 
+    j = search(tails, x)
 
-    if j > 0:
-        prev_idx[i] = tails_idx[j - 1]  
-    else:
-        prev_idx[i] = -1
+    prev_idx[i] = tails_pos[j - 1] if j > 0 else -1
 
-    if j == len(tails):
+    if j == len(tails): 
         tails.append(x)
-        tails_idx.append(i)
-    else:
+        tails_pos.append(i)
+    else:    
         tails[j] = x
-        tails_idx[j] = i
-
-    pos[i] = j  
+        tails_pos[j] = i
 
 lis_len = len(tails)
+k = tails_pos[-1] if tails_pos else -1
 
 keep = [False] * n
-need = lis_len - 1  
-last_b = float('inf')  
-
-for i in range(n - 1, -1, -1):
-    if pos[i] == need and brr[i] < last_b:
-        keep[i] = True
-        last_b = brr[i]
-        need -= 1
-        if need < 0:
-            break
-
+k = tails_pos[-1] if tails_pos else -1
+while k != -1:
+    keep[k] = True
+    k = prev_idx[k]
+    
 ans = [arr[i][0] for i in range(n) if not keep[i]]
 ans.sort()
-
+        
 print(n - lis_len)
 for a in ans:
     print(a)
