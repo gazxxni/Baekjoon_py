35d9dd55317466f3cdc73108042d85b8473e4ff3
gazxxni <jungsj0524@gmail.com>
2025-10-01 23:25:29 +0900

    a

--- my_local/10000~20000/11000/11653.py
+++ my_local/10000~20000/11000/11653.py
@@ -1,36 +1,17 @@
 import sys
+import math
 input = sys.stdin.readline
 
 n = int(input())
-arr = [True] * 10000001
-arr[0], arr[1] = False, False
 
-for i in range(2, int(n ** 0.5) + 1): 
-    if arr[i]:
-        for j in range(i * i, n + 1, i):
-            arr[j] = False
-
-brr = []           
-for i in range(n):
-    if arr[i]:
-        brr.append(i)
-
-a = 0
-
-if n == 2:
+while n % 2 == 0:
     print(2)
-    exit()
-elif n == 3:
-    print(3)
-    exit()
-    
-while True:
-    if n % brr[a] == 0:
-        n /= brr[a]
-        print(brr[a])
-        
-    else:
-        a += 1
-        
-    if n == 1:
-        break
+    n //= 2
+
+for i in range(3, int(math.sqrt(n)) + 1, 2):
+    while n % i == 0:
+        print(i)
+        n //= i
+
+if n > 1:
+    print(n)
