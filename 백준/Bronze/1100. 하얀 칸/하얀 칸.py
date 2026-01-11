26e5bcbce00fe0597e05d31e25241f81720ed9af
Sejun Jung <jungsj0524@gmail.com>
2026-01-11 13:14:39 +0900

    [Bronze II] Title: 하얀 칸, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,10 @@
+graph = [input().rstrip() for _ in range(8)]
+
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 1 and j % 2 == 1):
+            if graph[i][j] == 'F':
+                cnt += 1
+                
+print(cnt)
