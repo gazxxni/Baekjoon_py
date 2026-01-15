b6991b2b30265eb828889c911f78b00366ce5240
gazxxni <jungsj0524@gmail.com>
2026-01-15 15:50:49 +0900

    2420

--- my_local/1~10000/1000/1308.py
+++ my_local/1~10000/1000/1308.py
@@ -6,10 +6,8 @@ n_year, n_month, n_day = map(int, input().split())
 today = datetime.date(t_year, t_month, t_day)
 dday = datetime.date(n_year, n_month, n_day)
 
-gap = dday - today
-limit_date = datetime.date(t_year + 1000, t_month, t_day)
-
-if dday >= limit_date:
-    print("gg")
+if (n_year, n_month, n_day) >= (t_year + 1000, t_month, t_day):
+    print('gg')
 else:
-    print(f"D-{gap.days}")
+    gap = dday - today
+    print(f'D-{gap.days}')
