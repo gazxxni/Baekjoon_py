c8b7be5b7ea49f4f5e5f3323f0cb96d760e84693
Sejun Jung <jungsj0524@gmail.com>
2026-02-16 23:01:47 +0900

    [Silver V] Title: 숫자 빈도수, Time: 152 ms, Memory: 134980 KB -BaekjoonHub

---
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+n, d = map(int, input().split())
+arr = []
+for i in range(1, n + 1):
+    if i < 10:
+        arr.append(i)
+    elif 10 <= i < 100:
+        arr.append(int(str(i)[0]))
+        arr.append(int(str(i)[1]))
+    elif 100 <= i < 1000:
+        arr.append(int(str(i)[0]))
+        arr.append(int(str(i)[1]))
+        arr.append(int(str(i)[2]))
+    elif 1000 <= i < 10000:
+        arr.append(int(str(i)[0]))
+        arr.append(int(str(i)[1]))
+        arr.append(int(str(i)[2]))
+        arr.append(int(str(i)[3]))
+    elif 10000 <= i < 100000:
+        arr.append(int(str(i)[0]))
+        arr.append(int(str(i)[1]))
+        arr.append(int(str(i)[2]))
+        arr.append(int(str(i)[3]))
+        arr.append(int(str(i)[4]))
+    else:
+        arr.append(int(str(i)[0]))
+        arr.append(int(str(i)[1]))
+        arr.append(int(str(i)[2]))
+        arr.append(int(str(i)[3]))
+        arr.append(int(str(i)[4]))
+        arr.append(int(str(i)[5]))
+        
+cnt = Counter(arr)
+print(cnt[d])
