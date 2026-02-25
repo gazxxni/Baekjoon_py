9d3a3bce1256c6b8dd393e9e56032051dc2d2891
Sejun Jung <jungsj0524@gmail.com>
2026-02-26 00:20:13 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,4 @@
+SELECT  a.TITLE, a.BOARD_ID, b.REPLY_ID, b.WRITER_ID, b.CONTENTS, date_format(b.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+from USED_GOODS_BOARD a join USED_GOODS_REPLY b on a.BOARD_ID = b.BOARD_ID
+where year(a.CREATED_DATE) = 2022 and month(a.CREATED_DATE) = 10
+order by b.CREATED_DATE, a.TITLE
