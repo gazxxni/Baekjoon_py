2f2c619dc0ed6e0deaecbc360d1e489c9752ff02
Sejun Jung <jungsj0524@gmail.com>
2026-03-04 20:22:10 +0900

    [level 2] Title: 타겟 넘버, Time: 528.46 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+    def dfs(index, cur_sum):
+        if index == len(numbers):
+            if cur_sum == target:
+                return 1
+            else:
+                return 0
+            
+        return dfs(index + 1, cur_sum + numbers[index]) + dfs(index + 1, cur_sum - numbers[index])
+    
+    
+    return dfs(0, 0)
