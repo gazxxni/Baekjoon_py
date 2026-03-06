0837f1fac9f0739e91b4869a0a357e262bbf3ce7
Sejun Jung <jungsj0524@gmail.com>
2026-03-06 14:36:19 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,17 @@
+def solution(name):
+    up_down = 0
+    for c in name:
+        up_down += min(ord(c) - ord('A'), ord('Z') - ord(c) + 1)
+        
+    n = len(name)
+    move = n - 1
+    
+    for i in range(n):
+        next_i = i + 1
+        
+        while next_i < n and name[next_i] == 'A':
+            next_i += 1
+            
+        move = min(move, i * 2 + n - next_i, i + 2 * (n - next_i))
+    
+    return up_down + move
